d6d3b833ca6a4867d1ec1a619eba6b8094a61ed3
Magdalena Tyszkiewicz-Konopka <magdalena.tyszkiewicz-konopka@capgemini.com>
2024-06-14 14:07:01 +0200

    first AAA implementation

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -3,12 +3,20 @@ import { test, expect } from '@playwright/test';
 test.describe('User login to Demobank', () => {
 
   test('successful login with correct credentials', async ({ page }) => {
-    await page.goto('https://demo-bank.vercel.app/');
-    await page.getByTestId('login-input').fill('testerka');
-    await page.getByTestId('password-input').fill('12345678');
+    // Arrange
+    const url = 'https://demo-bank.vercel.app/';
+    const userName = 'testerka';
+    const userPassword = '12345678';
+    const expectedUserName = 'Jan Demobankowy';
+
+    // Act
+    await page.goto(url);
+    await page.getByTestId('login-input').fill(userName);
+    await page.getByTestId('password-input').fill(userPassword);
     await page.getByTestId('login-button').click();
 
-    await expect(page.getByTestId('user-name')).toHaveText('Jan Demobankowy');
+    // Assert
+    await expect(page.getByTestId('user-name')).toHaveText(expectedUserName);
   });
 
   test('unsuccessful login with too short username', async ({ page }) => {
