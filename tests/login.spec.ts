496bf69b003aff69c121a156818f774e56af186f
Magdalena Tyszkiewicz-Konopka <magdalena.tyszkiewicz-konopka@capgemini.com>
2024-06-14 10:36:42 +0200

    Refactor redundant click steps

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -4,19 +4,15 @@ test.describe('User login to Demobank', () => {
 
   test('successful login with correct credentials', async ({ page }) => {
     await page.goto('https://demo-bank.vercel.app/');
-    await page.getByTestId('login-input').click();
     await page.getByTestId('login-input').fill('testerka');
-    await page.getByTestId('password-input').click();
     await page.getByTestId('password-input').fill('12345678');
     await page.getByTestId('login-button').click();
-    await page.getByTestId('user-name').click();
 
     await expect(page.getByTestId('user-name')).toHaveText('Jan Demobankowy');
   });
 
   test('unsuccessful login with too short username', async ({ page }) => {
     await page.goto('https://demo-bank.vercel.app/');
-    await page.getByTestId('login-input').click();
     await page.getByTestId('login-input').fill('tester');
     await page.getByTestId('password-input').click();
 
@@ -25,9 +21,7 @@ test.describe('User login to Demobank', () => {
 
   test('unsuccessful login with too short password', async ({ page }) => {
     await page.goto('https://demo-bank.vercel.app/');
-    await page.getByTestId('login-input').click();
     await page.getByTestId('login-input').fill('testerka');
-    await page.getByTestId('password-input').click();
     await page.getByTestId('password-input').fill('12345');
     await page.getByTestId('password-input').blur();
 
